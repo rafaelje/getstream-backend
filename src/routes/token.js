6c570f23063eb4aa2572773910fbb61fbf424af1
Rafael Hernandez <rafael@elevatestartup.com>
2020-01-14 14:30:30 -0700

    update channel

--- src/routes/token.js
+++ src/routes/token.js
@@ -4,6 +4,6 @@ import { channel } from "../controllers/v1/channel";
 import { wrapAsync } from "../utils/controllers";
 
 module.exports = api => {
-  api.route("/v1/token").post(wrapAsync(token));
-  //api.route("/v1/channel").post(wrapAsync(channel));
+  //api.route("/v1/token").post(wrapAsync(token));
+  api.route("/v1/channel").post(wrapAsync(channel));
 };
