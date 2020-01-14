e617dfca93d1b1685573cd676ef86680e0e4c127
Rafael Hernandez <rafael@elevatestartup.com>
2020-01-14 14:18:01 -0700

    set route

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,7 +1,9 @@
 import { token } from "../controllers/v1/token";
+import { channel } from "../controllers/v1/channel";
 
 import { wrapAsync } from "../utils/controllers";
 
 module.exports = api => {
   api.route("/v1/token").post(wrapAsync(token));
+  api.route("/v1/channel").post(wrapAsync(channel));
 };
